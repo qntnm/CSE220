783f44f7963c2dd81cdf27cb56f8d7348b7dfb2a
qntnm <hussemibrahim@gmail.com>
2024-10-06 21:23:01 -0700

    intro to A3 with psuedo code

---
+++ assignments/assignment3/wordgame.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// prints init string then & handles quit or continue
+void playGame(char* guess[]);
+// checks if the guess is correct. (0 is false 1 is true)
+int checkGuess(char guess);
+// returns letter placment e.g. _e__e_ when the word is letter and the guess was e. 
+//If the guess not in the word, just prints letter length in underscores
+char guessPlacment(char guess);
+
+
+// Calls play game
+int main(int numArguments, char* argumentValue[]){
+
+}
