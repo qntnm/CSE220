edf6ae7dc5053d75020692085e8f4d092d59ac2c
qntnm <hussemibrahim@gmail.com>
2024-10-08 16:12:05 -0700

    wip

--- assignments/assignment3/wordgame.c
+++ assignments/assignment3/wordgame.c
@@ -1,14 +1,78 @@
 #include <stdio.h>
+#include <string.h>
+#define TRUE 1
+#define FALSE 0
 // prints init string then & handles quit or continue
-void playGame(char* guess[]);
+void playGame(char *word);
 // checks if the guess is correct. (0 is false 1 is true)
 int checkGuess(char guess);
 // returns letter placment e.g. _e__e_ when the word is letter and the guess was e. 
 //If the guess not in the word, just prints letter length in underscores
-char guessPlacment(char guess);
+char guessPlacment(char guess, char* word,char* placementWord[]);
+
 
 
 // Calls play game
+// first argument value is always file path
 int main(int numArguments, char* argumentValue[]){
+    char quit = 0;
+    int gameEnded = 0;
+   
+    
+    // Check if game has ended
+    while(!gameEnded){
+        // check if they want to quit
+        scanf("%c",&quit);
+        if(quit == 'q'){
+            gameEnded = 1;
+            break;
+        }else{
+            playGame();
+
+        }
+            
+
+        
+    }
+    
+}
+void playGame(char* word){
+    
+    
+    
 
 }
+char 
+
+char guessPlacment(char guess,char* word,char* placementWord[]){
+    char *guessptr = &guess;
+    int guessLength = strlen(guessptr);
+    int wordLength = strlen(word);
+    char underscorePlace[wordLength];
+    //char wordArr[] = *word;
+    // if the guess is a word or letter
+    if(guessLength > 1){
+        // check if the word is equal to the word guess.
+        if(guess == *word){
+            return guess;
+        }else{
+            return 0;
+        }
+        // guess is a letter
+    }else{
+     for(int i=0; i<wordLength; i++){
+        if(guess == word[i]){
+        *placementWord[i] = guess;
+        }else{
+            *placementWord[i] = '_';
+        }
+     }
+    
+    }
+
+}
+
+
+
+    
+
---
+++ lectures/10-07-24/intro.md
@@ -0,0 +1,3 @@
+linked list is a struct possibly that has 2 values, some data and the pointer to the next node. In the stack, the pointers of the nodes are not coninous could go from 0x0 to 0x8.
+a struct with an int and a pointer is 12 bytes. 4 for the int and usally 8 for the pointer.
+to check if a list is empty head = null, if it has 1 element it has a head but tail is the same as head.
